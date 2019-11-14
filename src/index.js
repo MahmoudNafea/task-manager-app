973d30ff2b0efe866f533c937bf96f35953b316a
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-11-14 20:04:58 +0800

    add options filtering paginating and sorting

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ app.listen(port, () => {
     console.log("App's running on port " + port)
 })
 
-const User = require('./models/user')
+// const User = require('./models/user')
 
-const main = async () => {
-    const user = await User.findById("5dc922fe6fea614a943b72ae")
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-main()
+// const main = async () => {
+//     const user = await User.findById("5dc922fe6fea614a943b72ae")
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+// main()
